5ec1ad41d15181727d759f977acb41bb439ad391
nina2189 <g994867@yahoo.com.tw>
2026-08-11 13:54:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ LINE_TOKEN = "ovPcLgBzyxMsqwLjx2d/AToVtViPzXgf1r0rcrwNpf/B9eJG6M92FI3a1LK7daQgft
 USER_ID = "U1536e6ab5269bfd96c67970dcb4092ef"
 
 def main():
-    url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastores/F-C0032-001?Authorization=CWA-B969C6D1-AF9D-46C1-B2A2-8B3F25A3F7A1"
+    url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastores/F-C0032-001?Authorization=CWA-C2A9D003-668C-4B88-9A43-952B87902D00"
     response = requests.get(url, verify=False).json()
 
     all_locations = response["records"]["location"]
